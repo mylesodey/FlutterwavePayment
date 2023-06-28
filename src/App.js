58693d62e9e0ba537bb658e4bf48c0be78243986
Mete Oyama <48647173+mylesodey@users.noreply.github.com>
2023-06-28 12:01:15 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useFlutterwave, closePaymentModal } from 'flutterwave-react-v3'
 import './App.css';
+import flutter from "./images/Flutterwave-New-2022.svg";
+import paystack from "./images/paystack-2.svg"
 
 function App() {
+  const config = {
+    public_key: 'FLWPUBK_TEST-089a59f302d6f9f5e4343293ad927f3e-X',
+    tx_ref: Date.now(),
+    amount: 100,
+    currency: 'NGN',
+    payment_options: 'card,mobilemoney,ussd',
+    customer: {
+      email: 'user@gmail.com',
+      phone_number: '070********',
+      name: 'john doe',
+    },
+    customizations: {
+      title: 'My store',
+      description: 'Payment for items in cart',
+      logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
+    },
+  };
+
+  // const fwConfig = {
+  //   ...config,
+  //   text: 'Pay with Flutterwave!',
+  //   callback: (response) => {
+  //      console.log(response);
+  //     closePaymentModal() // this will close the modal programmatically
+  //   },
+  //   onClose: () => {},
+  // };
+
+  const handleFlutterPayment = useFlutterwave(config);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Choose Payment Option</h1>
+      <div className="options">
+        <button className='paystack'><a href="https://paystack.com/pay/-3ncb77lzi"><img className='payi' src={paystack} alt="paystack" /></a></button>
+
+
+        <button className='flutter'
+          onClick={() => {
+            handleFlutterPayment({
+              callback: (response) => {
+                console.log(response);
+                closePaymentModal() // this will close the modal programmatically
+              },
+              onClose: () => { },
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <img className='payi' src={flutter} alt="flutterwave" />
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Config.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useFlutterwave, closePaymentModal  } from 'flutterwave-react-v3'
+function Config() {
+    const config = {
+        public_key: 'FLWPUBK-**************************-X',
+        tx_ref: Date.now(),
+        amount: 100,
+        currency: 'NGN',
+        payment_options: 'card,mobilemoney,ussd',
+        customer: {
+          email: 'user@gmail.com',
+          phone_number: '070********',
+          name: 'john doe',
+        },
+        customizations: {
+          title: 'My store',
+          description: 'Payment for items in cart',
+          logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
+        },
+      };
+    
+      const fwConfig = {
+        ...config,
+        text: 'Pay with Flutterwave!',
+        callback: (response) => {
+           console.log(response);
+          closePaymentModal() // this will close the modal programmatically
+        },
+        onClose: () => {},
+      };
+    
+  return (
+    <div className="App">
+     <h1>Hello Test user</h1>
+      <button {...fwConfig} >Flutterwave</button>
+    </div>
+  )
+}
+
+export default Config
